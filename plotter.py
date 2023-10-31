11daf70a452b28ad87ce563e7da3d907cb76cdfc
jjklq <148258631+jjklq@users.noreply.github.com>
2023-10-31 11:24:58 +0200

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -1,26 +1,32 @@
 import requests
 import matplotlib.pyplot as plt
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 
+# Функция для получения данных о курсе UAH/USD с 2000 года до текущей даты
 def fetch_exchange_rate_data():
-    url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=USD&date=20000101&json'
-    response = requests.get(url)
-    data = response.json()
+    start_date = datetime(2000, 1, 1)
+    end_date = datetime.now()
 
-    # Инициализация списков для данных
     dates = []
     exchange_rates = []
 
-    for entry in data:
-        date = entry['exchangedate']
-        rate = entry['rate']
-        dates.append(date)
-        exchange_rates.append(rate)
+    current_date = start_date
+    while current_date <= end_date:
+        date_str = current_date.strftime('%d.%m.%Y')
+        url = f'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=USD&date={date_str}&json'
+        response = requests.get(url)
+        data = response.json()
+        if data:
+            date = data[0]['exchangedate']
+            rate = data[0]['rate']
+            dates.append(date)
+            exchange_rates.append(rate)
+        current_date += timedelta(days=1)
 
     return list(zip(dates, exchange_rates))
 
-
+# Функция для построения графика
 def plot_exchange_rate(data):
     df = pd.DataFrame(data, columns=['Date', 'Exchange Rate'])
     df['Date'] = pd.to_datetime(df['Date'], format='%d.%m.%Y')
